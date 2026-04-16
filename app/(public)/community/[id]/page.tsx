feced18e4d15968f7bf2417f6a82006222cfd4ff
Victor-Okenwa <okenwavictor003@gmail.com>
2026-04-16 23:59:16 +0100

    feat: enhance CommunityPage with detailed community information and member-only content
    - Added a badge to display the number of community members.
    - Improved community description styling and added placeholder text for better user engagement.
    - Introduced a section for community rules with conditional rendering based on availability.
    - Implemented a toggle feature for viewing hidden content, enhancing user interaction with community-specific resources.

--- app/(public)/community/[id]/page.tsx
+++ app/(public)/community/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useApp } from "@/components/providers/app-provider";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Spinner } from "@/components/ui/spinner";
+import { cn } from "@/lib/utils";
 import { CommunityDetail } from "@/utils/constants";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 
 export default function CommunityPage() {
@@ -11,6 +14,7 @@ export default function CommunityPage() {
     const [communityDetails, setCommunityDetails] = useState<CommunityDetail | null>(null);
     const [error, setError] = useState<string | null>(null);
     const [reloadToken, setReloadToken] = useState(0);
+    const [viewSecret, setViewSecret] = useState(false);
 
     const { userAddress } = useApp()
 
@@ -32,22 +36,24 @@ export default function CommunityPage() {
                 params.set("community_id", id);
                 if (userAddress) params.set("user_address", userAddress);
 
-                const controller = new AbortController();
-                const timeout = setTimeout(() => controller.abort(), 15_000); // 15 seconds timeout
+                // const controller = new AbortController();
+                // const timeout = setTimeout(() => controller.abort(), 15_000); // 15 seconds timeout
 
                 let res;
                 try {
                     res = await fetch(`/api/community/getCommunity?${params}`, {
-                        signal: controller.signal,
+                        // signal: controller.signal,
                     });
                 } finally {
-                    clearTimeout(timeout);
+                    // clearTimeout(timeout);
                 }
 
                 const json = await res.json();
                 if (!res.ok) throw new Error(json.error ?? "Failed to load communities");
 
-                const details = json.communities as CommunityDetail;
+                const details = json.community as CommunityDetail;
+
+                console.log(details)
                 setCommunityDetails(details);
             } catch (error) {
                 setError(error instanceof Error ? error.message : "Something went wrong")
@@ -85,15 +91,60 @@ export default function CommunityPage() {
             {!isLoading && !error && (
                 <article className="">
 
-                    <section className="flex flex-wrap gap-2 py-2 px-2">
-                        <hgroup className="space-y-2">
+                    <section className="flex flex-wrap gap-4 py-4 px-2 sm:px-4">
+                        <hgroup className="space-y-2 sm:flex-1">
                             <h1 className="text-2xl fonct-bold">{communityDetails?.name}</h1>
-                            <p className="text-muted sm:text-sm text-xs">{communityDetails?.description}</p>
+                            <p className="text-primary/60 sm:text-sm text-xs">{communityDetails?.description}
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, facere veniam. Consectetur eum quidem repudiandae fugit esse ad magnam mollitia hic, ipsum, ut delectus dolores vel. Obcaecati, temporibus voluptatem impedit doloribus beatae ducimus laudantium quo? Nemo laborum sed ut aut corporis quis veniam, nihil a quia. Repudiandae temporibus modi qui?
+                            </p>
                         </hgroup>
 
+                        <Badge variant={"secondary"} className="">{communityDetails?.membersCount} Member{communityDetails?.membersCount !== 1 ? "s" : ""}</Badge>
                     </section>
 
                     <Separator className="my-2" />
+
+                    <section className="px-2 sm:px-4 py-4 space-y-4">
+                        <h3 className="text-lg font-bold">Rules</h3>
+
+                        {
+                            communityDetails?.guidelines && communityDetails?.guidelines.length > 0 ? (
+                                <ul className=" space-y-1 *:text-primary/60 *:text-sm">
+                                    {
+                                        communityDetails.guidelines.map((rule) => <li key={rule.slice(0, 2)}> &bull; {rule}</li>)
+                                    }
+                                </ul>
+                            ) :
+                                (
+                                    <p className="text-muted">No Rule attached to this community</p>
+                                )
+                        }
+                    </section>
+
+                    <Separator className="my-2" />
+
+
+                    <section className="px-2 sm:px-4 py-4 space-y-4">
+                        <hgroup>
+                            <h3 className="text-lg font-bold">Members Only Content</h3>
+                            <p className="text-primary/60 text-sm">Click the button below to view hidden content</p>
+                        </hgroup>
+
+
+                        <div className="flex gap-2">
+                            <p className={cn("px-4 py-3 blur-sm border rounded-sm text-primary/70 pointer-events-none", {
+                                "blur-0 pointer-events-auto": viewSecret
+                            })}>{communityDetails?.hiddenLink}</p>
+                            <Button variant={"outline"} onClick={() => setViewSecret((prev) => !prev)} className="h-auto">
+                                {viewSecret ?
+                                    (<>
+                                        <EyeOffIcon /> Hide
+                                    </>) : (<>
+                                        <EyeIcon /> View
+                                    </>)}
+                            </Button>
+                        </div>
+                    </section>
 
                 </article>
             )}
